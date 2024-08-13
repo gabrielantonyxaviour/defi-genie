9d22658112aaaca1414cc2f94b3226ec2d7d9ae5
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-08-13 14:53:45 +0530

    add styles

--- frontend/src/components/ui/layout.tsx
+++ frontend/src/components/ui/layout.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import { Separator } from "@/components/ui/separator";
 import { SidebarNav } from "@/components/ui/side-bar";
+import { Button } from "./button";
 
 const sidebarNavItems = [
   {
@@ -33,28 +34,15 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-          <p className="text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
-          </p>
+      <div className="space-y-6 p-10 pb-16">
+        <div className="space-y-0.5 flex w-full justify-between">
+          <div className="flex">
+            <Image src={"/logo.png"} height={50} width={60} alt="Logo" />
+            <h2 className="text-2xl font-bold tracking-tight ml-2">
+              Defi.Genie
+            </h2>
+          </div>
+          <Button variant="outline">Connect Wallet</Button>
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
