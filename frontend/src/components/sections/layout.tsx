157b0c93da7aa7202b5009c506ea9acb0be888f0
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-08-26 17:39:33 +0530

    add ai sidebar

--- frontend/src/components/sections/layout.tsx
+++ frontend/src/components/sections/layout.tsx
@@ -12,22 +12,13 @@ import PasscodeDialog from "./passcode-dialog";
 import { Link } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { MainNav } from "./navbar";
+import AIComponent from "./ai";
 
-const sidebarNavItems = [
-  {
-    title: "Dashboard",
-    href: "/",
-  },
-  {
-    title: "Positions",
-    href: "/positions",
-  },
-
-  {
-    title: "Ask AI",
-    href: "/chat",
-  },
-];
+interface ClassifyResponse {
+  response: string;
+  action: string;
+  params: string;
+}
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -36,6 +27,11 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   const { status } = useAccount();
   const [passCode, setPasscode] = useState("");
+  const [classifyResponse, setClassifyResponse] = useState<ClassifyResponse>({
+    response: "",
+    action: "",
+    params: "",
+  });
   const [access, setAccess] = useState(true); // TODO: Turn this off
   return (
     <>
@@ -45,19 +41,27 @@ export default function Layout({ children }: LayoutProps) {
         setAccess={setAccess}
       /> */}
       {access && (
-        <div className="px-8 w-full h-screen flex flex-col">
-          <div className="flex w-full justify-between">
-            <div className="flex items-center py-6">
-              <Image src={"/logo.png"} height={50} width={50} alt="Logo" />
-              <MainNav className="mx-6" />
+        <div className="h-screen flex">
+          <div className="px-8 w-[75%] flex flex-col justify-center items-center">
+            <div className="flex w-full justify-between">
+              <div className="flex items-center py-6">
+                <Image src={"/logo.png"} height={50} width={50} alt="Logo" />
+                <MainNav className="mx-6" />
+              </div>
             </div>
-            <ConnectButton />
-          </div>
 
-          <div className="flex flex-1 space-x-12 w-full">
-            <div className="flex-1 flex flex-col w-full h-full">
-              {status != "connected" ? <DefaultLanding /> : children}
+            <div className="flex flex-1 space-x-12 w-full">
+              <div className="flex-1 flex flex-col w-full h-full">
+                {status != "connected" ? <DefaultLanding /> : children}
+              </div>
             </div>
+          </div>
+          <div className="flex-1 flex flex-col justify-center p-4 h-full bg-background border">
+            <ConnectButton />
+            <AIComponent
+              classifyResponse={classifyResponse}
+              setClassifyResponse={setClassifyResponse}
+            />
           </div>
         </div>
       )}
