c0959611db8e8b2c790060ee7a0bb70e25b3b0be
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-08-28 23:22:08 +0530

    smol

--- frontend/src/components/sections/default-landing.tsx
+++ frontend/src/components/sections/default-landing.tsx
@@ -4,7 +4,7 @@ export default function DefaultLanding() {
   return (
     <div className="flex-1 flex flex-col justify-center items-center">
       <h1 className="font-bold text-2xl">Welcome to Defi Genie</h1>
-      <p>An AI assistant for Liquidity Provision</p>
+      <p>An AI assistant for Defi Beginners</p>
       <div className="py-4">
         <ConnectButton />
       </div>
