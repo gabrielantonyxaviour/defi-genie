f3684287f7d370ffc50dc00363adff5440858bf6
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-08-28 19:51:53 +0530

    classify

--- frontend/src/app/api/classify/route.tsx
+++ frontend/src/app/api/classify/route.tsx
@@ -1,7 +1,7 @@
 "use server";
 import axios from "axios";
 import { NextRequest } from "next/server";
-const url = "https://d523-14-194-86-232.ngrok-free.app/start-chat";
+const url = "https://defi-genie.onrender.com/start-chat";
 export async function POST(req: NextRequest, res: Response) {
   try {
     const body = await req.json();
